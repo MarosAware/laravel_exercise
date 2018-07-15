73ecedc3c579a90a3bbe9796bd10a5442913b0b5
MarosAware <marosdevone@gmail.com>
2018-07-15 16:41:26 +0200

    Testing.

--- app/Post.php
+++ app/Post.php
@@ -52,6 +52,7 @@ class Post extends Model
         return static::selectRaw('year(created_at) year, monthname(created_at) month, count(*) published')
             ->groupBy('year', 'month')
             ->orderByRaw('min(created_at) desc')
-            ->get();
+            ->get()
+            ->toArray();
     }
 }
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -4,9 +4,12 @@ namespace Tests\Unit;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Post;
 
 class ExampleTest extends TestCase
 {
+
+    use RefreshDatabase;
     /**
      * A basic test example.
      *
@@ -14,6 +17,34 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $this->assertTrue(true);
+        //$this->get('/')->assertSee('Title of a longer featured blog post');
+
+        //Given I have two records in the database that are posts, and each one is posted a month apart
+
+        $first = factory(Post::class)->create();
+        $second = factory(Post::class)->create([
+            'created_at' => \Carbon\Carbon::now()->subMonth(2)->subYear()
+        ]);
+
+        //When I fetch the archives.
+        $posts = Post::archives();
+
+        //dd($posts);
+        //Then the response should be in the proper format.
+        $this->assertEquals([
+            [
+                "year" => $first->created_at->format('Y'),
+                "month" => $first->created_at->format('F'),
+                "published" => 1
+
+            ],
+
+            [
+                "year" => $second->created_at->format('Y'),
+                "month" => $second->created_at->format('F'),
+                "published" => 1
+
+            ]
+        ], $posts);
     }
 }
